119761e0349ca0859bedb0b30bada3ecc1c1d3b5
a1sya <azsya99@gmail.com>
2022-11-28 04:47:41 +0800

    add home page hero section

---
+++ frontend/src/components/pages/HomePage/ProductHeroLayout.jsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+
+const ProductHeroLayoutRoot = styled('section')(({ theme }) => ({
+    color: theme.palette.common.white,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    [theme.breakpoints.up('sm')]: {
+        height: '70vh',
+        minHeight: 500,
+        maxHeight: 1300,
+    },
+}));
+
+const Background = styled(Box)({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    zIndex: -2,
+});
+
+const ProductHeroLayout = (props) => {
+    const { sxBackground, children } = props;
+
+    return (
+        <ProductHeroLayoutRoot>
+            <Container
+                sx={{
+                    my: 14,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    justifyContent: 'center',
+                    flex: 1,
+                }}
+            >
+                {children}
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        backgroundColor: 'common.black',
+                        opacity: 0.5,
+                        zIndex: -1,
+                    }}
+                />
+                <Background sx={sxBackground} />
+            </Container>
+        </ProductHeroLayoutRoot>
+    );
+};
+
+export default ProductHeroLayout;
--- frontend/src/components/pages/HomePage/index.jsx
+++ frontend/src/components/pages/HomePage/index.jsx
@@ -1,9 +1,49 @@
-import { Stack, Typography } from '@mui/material';
+import * as React from 'react';
+
+import { Typography, Button } from '@mui/material';
+
+import Background from 'assets/images/HPbrng.jpg';
+
+import ProductHeroLayout from './ProductHeroLayout';
+
+// this is where the home page codes will be
+// we need some simple home page similar to quizlet
 
 export const HomePage = () => {
     return (
-        <Stack>
-            <Typography variant="body2">HomePage</Typography>
-        </Stack>
+        <ProductHeroLayout
+            sxBackground={{
+                background: `url(${Background})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+            }}
+        >
+            <Typography color="inherit" textAlign="left" variant="h2">
+                The best digital flashcards and study tools
+            </Typography>
+            <Typography
+                color="inherit"
+                textAlign="left"
+                variant="h6"
+                sx={{ mb: 4, mt: { xs: 4, sm: 8 } }}
+            >
+                Join approximately more than 60 million students who use Flip
+                Quiz flashcards, exam prep, and professional solutions to
+                enhance their GPAs and accomplish their objectives.
+            </Typography>
+            <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                component="a"
+                href="/premium-themes/onepirate/sign-up/"
+                sx={{ minWidth: 200 }}
+            >
+                Sign up for free
+            </Button>
+            <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+                Discover the experience
+            </Typography>
+        </ProductHeroLayout>
     );
 };
