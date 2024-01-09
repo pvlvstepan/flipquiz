57abe458ef5aa8aff9bd1f015e92af8d552de3cc
Stepan Pavlov <78949269+pvlvstepan@users.noreply.github.com>
2024-01-09 20:25:46 +0800

    fix: loading screens

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -10,9 +10,11 @@ export default function HomeLayout({ children }: HomeLayoutProps) {
     return (
         <div className="flex flex-1 flex-col bg-primary/5">
             <Header />
-            <div className="flex-1 overflow-x-hidden">
-                <div className="flex h-full flex-col">
-                    <main className="flex-1 py-8">{children}</main>
+            <div className="flex flex-1 flex-col overflow-x-hidden">
+                <div className="flex flex-1 flex-col">
+                    <main className="flex flex-1 flex-col py-8">
+                        {children}
+                    </main>
                 </div>
             </div>
             <div className="container mt-auto">
--- src/app/(home)/profile/[id]/loading.tsx
+++ src/app/(home)/profile/[id]/loading.tsx
@@ -5,7 +5,7 @@ import { loadingScreenTitles } from "@/lib/const";
 
 export default function Loading() {
     return (
-        <div className="flex h-full flex-col items-center justify-center py-10 text-primary">
+        <div className="flex flex-1 flex-col items-center justify-center py-10 text-primary">
             <Spinner className="text-2xl" />
             <span className="mt-4 max-w-lg text-center text-lg text-muted-foreground">
                 {faker.helpers.arrayElement(loadingScreenTitles)}
