fdfe6f13587f85a8c1cd2d00185081e6ee1fb5ac
Stepan Pavlov <78949269+pvlvstepan@users.noreply.github.com>
2024-01-09 19:58:24 +0800

    fix: type errors

--- src/app/(home)/_components/sections/study-set/comments/actions.ts
+++ src/app/(home)/_components/sections/study-set/comments/actions.ts
@@ -1,7 +1,5 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-
 import type {
     addCommentInput,
     deleteCommentInput,
@@ -13,18 +11,12 @@ import { api } from "@/trpc/server";
 export const deleteComment = async (
     data: z.infer<typeof deleteCommentInput>,
 ) => {
-    return api.studySet.comment.deleteComment.mutate(data).then((comment) => {
-        revalidatePath(`/study-set/${comment.studySetId}`);
-    });
+    void api.studySet.comment.deleteComment.mutate(data);
 };
 
 export const addComment = async (data: z.infer<typeof addCommentInput>) => {
-    return api.studySet.comment.addComment
-        .mutate({
-            content: data.content.replace(/\n{2,}/g, "\n"),
-            studySetId: data.studySetId,
-        })
-        .then((comment) => {
-            revalidatePath(`/study-set/${comment.studySetId}`);
-        });
+    void api.studySet.comment.addComment.mutate({
+        content: data.content.replace(/\n{2,}/g, "\n"),
+        studySetId: data.studySetId,
+    });
 };
