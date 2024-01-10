96a8889470dd9e610d04811a83450cc0f159dd57
Stepan Pavlov <78949269+pvlvstepan@users.noreply.github.com>
2024-01-10 15:15:41 +0800

    fix: comment revalidation

--- src/app/(home)/_components/sections/study-set/comments/actions.ts
+++ src/app/(home)/_components/sections/study-set/comments/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 import type {
     addCommentInput,
     deleteCommentInput,
@@ -11,12 +13,18 @@ import { api } from "@/trpc/server";
 export const deleteComment = async (
     data: z.infer<typeof deleteCommentInput>,
 ) => {
-    void api.studySet.comment.deleteComment.mutate(data);
+    void api.studySet.comment.deleteComment.mutate(data).then(() => {
+        revalidatePath("");
+    });
 };
 
 export const addComment = async (data: z.infer<typeof addCommentInput>) => {
-    void api.studySet.comment.addComment.mutate({
-        content: data.content.replace(/\n{2,}/g, "\n"),
-        studySetId: data.studySetId,
-    });
+    void api.studySet.comment.addComment
+        .mutate({
+            content: data.content.replace(/\n{2,}/g, "\n"),
+            studySetId: data.studySetId,
+        })
+        .then(() => {
+            revalidatePath("");
+        });
 };
